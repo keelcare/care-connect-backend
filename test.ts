2b7e107dabd3d1ad1cbce5aea73291acbf2ede2d
Chhuanga <120743913+Chhuanga@users.noreply.github.com>
2026-06-06 01:38:20 +0530

    chore: unused files removed

--- test.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-prisma.service_requests.findMany({
-  where: {
-    bookings: {
-      isNot: null
-    }
-  }
-});
--- test_nannies.sql
+++
@@ -1,5 +0,0 @@
-SELECT u.id, p.first_name, p.lat, p.lng, nd.is_available_now, u.is_verified
-FROM users u
-JOIN profiles p ON u.id = p.user_id
-JOIN nanny_details nd ON u.id = nd.user_id
-WHERE u.role = 'nanny';
